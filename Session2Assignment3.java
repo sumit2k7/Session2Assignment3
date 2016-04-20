b6a1af4b42ea70058b5f2a9e32300e3c18d5106b
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-20 13:26:53 +0530

    Added files via upload

---
+++ Session2Assignment3.java
@@ -0,0 +1,25 @@
+
+public class Session2Assignment3 {
+
+	public static void main(String[] args) {
+		
+	for(int i=1;i<=5;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+			System.out.print(j);
+			}
+			System.out.println();
+	}
+		
+		for(int i=4;i>=1;i--)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		}
+}
